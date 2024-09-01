5e6b6840553b1d320a8b7d4f80ff8ea859c9ce70
RisabKshetri <155343200+risaw33@users.noreply.github.com>
2024-09-01 18:20:29 +0530

    basic hero section added

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
-// import SidebarDemo from "@/components/SidebarDemo";
+import Homepage from "@/components/Homepage";
 
 export default function Home() {
   return (
     <>
     <Navbar />
-    {/* <SidebarDemo /> */}
+    <div className="px-32">
+    <Homepage />
+    </div>
     </>
   )
 }
