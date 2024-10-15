f40eaa2bd7d518eb3541b6e7ebaa40fb2ec7f4a4
risabkshetri <risawgc@gmail.com>
2024-10-15 11:11:35 +0530

    blog data loading error fixed

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
     images: {
         domains: ['assets.aceternity.com', 'images.unsplash.com', 'picsum.photos', 'example.com', 'i.imgur.com', 'res.cloudinary.com'],
     },
+    staticPageGenerationTimeout: 300,
 };
 
 export default nextConfig;
